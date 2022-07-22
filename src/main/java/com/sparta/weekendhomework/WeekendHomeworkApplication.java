15c08d6c076220f7f4131693cc6e1b67cb09f915
Rzreo <zuzu5313@naver.com>
2022-07-22 16:35:45 +0900

    기본파일

---
+++ src/main/java/com/sparta/weekendhomework/WeekendHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.weekendhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekendHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/weekendhomework/WeekendHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.weekendhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekendHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
